6fa27d19b49a3d0216c52452b336204b5d3cd38f
velina26staneva <velin4eto01@gmail.com>
2020-10-22 15:13:26 +0000

    1

--- src/exercise4/MaxElement.java
+++ src/exercise4/MaxElement.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MaxElement {
-  public static void main(String [] args) {
+  public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
 
     int a = input.nextInt();
-    double[] numbers = new double[a];
-    double Max = numbers[0];
+    double [] numbers = new double[a];
 
     System.out.print("Enter a number: ");
 
     for(int index = 0; index < numbers.length; index++) {
         numbers[index] = input.nextDouble();
     }
+    double Max = numbers[0];
     for(int index = 1; index < numbers.length; index++) {
       if(Max < numbers[index]) {
         Max = numbers[index];
