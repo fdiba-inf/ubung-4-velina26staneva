635ebb3b024a5057e1654cbd6ccaa24786f8ddb4
velina26staneva <velin4eto01@gmail.com>
2020-10-22 15:01:15 +0000

    1

--- src/exercise4/MaxElement.java
+++ src/exercise4/MaxElement.java
@@ -10,13 +10,13 @@ public class MaxElement {
 
     int a = input.nextInt();
     int[] numbers = new int[a];
-    double Max = numbers[0];
 
     System.out.print("Enter a number: ");
 
     for(int index = 0; index < numbers.length; index++){
         numbers[index] = input.nextInt();
     }
+    double Max = numbers[0];
     for(int index = 1; index < numbers.length; index++){
       if(Max < numbers[index]) {
         Max = numbers[index];
